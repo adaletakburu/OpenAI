a1f10c038ef7fd672a618db19204e27af6dfd744
Adalet Akburu <adalet.akburu@hotmail.com>
2023-04-10 00:13:02 +0300

    Add project files.

---
+++ OpenAI.WebApi/Controllers/OpenAIController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels.ResponseModels;
+using OpenAI.GPT3.ObjectModels.ResponseModels.ImageResponseModel;
+
+namespace OpenAI.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAIController : ControllerBase
+    {
+
+        private readonly IOpenAIService openAIService;
+
+        public OpenAIController(IOpenAIService openAIService)
+        {
+            this.openAIService = openAIService;
+        }
+
+        [HttpGet]
+        [Route("askQuestion")]
+        public async Task<ActionResult> GetChat(string question)
+        {
+            CompletionCreateResponse response = await openAIService.Completions.CreateCompletion(new CompletionCreateRequest()
+            {
+                Prompt = question,
+                MaxTokens = 500
+            }, Models.TextDavinciV3);
+
+            return Ok(response.Choices[0].Text);
+        }
+
+        [HttpGet]
+        [Route("imageGenarate")]
+        public async Task<ActionResult> GetImageGenerator(string imageGenarate)
+        {
+            ImageCreateResponse response = await openAIService.Image.CreateImage(new ImageCreateRequest()
+            {
+                Prompt = imageGenarate,
+                N = 2,
+                Size = StaticValues.ImageStatics.Size.Size256,
+                ResponseFormat = StaticValues.ImageStatics.ResponseFormat.Url,
+                User = "Test"
+            });
+            return Ok(string.Join("\n\n", response.Results.Select(i => i.Url)));
+        }
+
+    }
+}
---
+++ OpenAI.WebApi/Program.cs
@@ -0,0 +1,31 @@
+using OpenAI.GPT3.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenAIService(settings => { settings.ApiKey = "**--APÝ--KEY--**"; });
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
